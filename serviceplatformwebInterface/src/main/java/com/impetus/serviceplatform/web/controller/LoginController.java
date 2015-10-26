f4c99b5ce9b9b310e9c39cf5b973117424a05e29
rakesh.singhania <rakesh.singhania@impetus-N408.impetus.co.in>
2015-10-26 22:45:28 +0530

    removed unnessary code

--- serviceplatformwebInterface/src/main/java/com/impetus/serviceplatform/web/controller/LoginController.java
+++ serviceplatformwebInterface/src/main/java/com/impetus/serviceplatform/web/controller/LoginController.java
@@ -8,14 +8,11 @@ import java.sql.SQLException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.impetus.process.LoginProcess;
@@ -24,41 +21,34 @@ import com.impetus.process.exception.ServicePlatformDBException;
 
 @Controller
 @SessionAttributes("LoggedInUser")
-public class LoginController {
-	Logger logger = LoggerFactory.getLogger(getClass());
-	@Autowired
-	private LoginProcess loginProcess;
-	private String status;
+public class LoginController
+{
+  Logger logger = LoggerFactory.getLogger(getClass());
+  @Autowired
+  private LoginProcess loginProcess;
+  private String status;
 
-	@RequestMapping(value = "/signup", method = RequestMethod.GET)
-	public String postService() {
-		return "signup";
-	}
+  @RequestMapping(value = "/signup", method = RequestMethod.GET)
+  public String postService()
+  {
+    return "signup";
+  }
 
-	@RequestMapping(value = { "/customLoginPage" }, method = RequestMethod.GET)
-	@ResponseStatus(HttpStatus.OK)
-	public String renderLoginForm(ModelMap model) {
-		return "customLoginPage";
-	}
-
-	@RequestMapping(value = "/loginSave", method = RequestMethod.POST)
-	public String saveLogin(@RequestBody LoginData loginData) {
-		logger.info(loginData.getEmail());
-		logger.info(loginData.getPassword());
-		return "views/loginPage";
-	}
-
-	@RequestMapping(value = "/RegisterUser/doLogin", method = RequestMethod.POST)
-	public @ResponseBody String loginUser(@RequestBody LoginData loginData)
-			throws ServicePlatformDBException {
-		try {
-			status = loginProcess.loginUser(loginData);
-		} catch (SQLException e) {
-			if (e instanceof SQLException)
-				throw new ServicePlatformDBException("SQL exception occured: "
-						+ e.getMessage());
-		}
-		return "{\"status\":\"" + status + "\"}";
-	}
+  @RequestMapping(value = "/RegisterUser/doLogin", method = RequestMethod.POST)
+  public @ResponseBody
+  String loginUser(@RequestBody
+  LoginData loginData) throws ServicePlatformDBException
+  {
+    try
+    {
+      status = loginProcess.loginUser(loginData);
+    }
+    catch (SQLException e)
+    {
+      if (e instanceof SQLException)
+        throw new ServicePlatformDBException("SQL exception occured: " + e.getMessage());
+    }
+    return "{\"status\":\"" + status + "\"}";
+  }
 
 }
