481838df8922eea9a38d63f898876cce9c546097
Saurabh Chandra Solanki <saurabhc.solanki@impetus.co.in>
2015-11-27 08:24:44 -0200

    Update RunTest.java

--- serviceplatformwebInterface/src/test/java/feature/sp/RunTest.java
+++ serviceplatformwebInterface/src/test/java/feature/sp/RunTest.java
@@ -5,7 +5,6 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class RunTest {
 
 }
