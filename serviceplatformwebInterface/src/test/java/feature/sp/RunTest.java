1af6a4a888b7479982456035ac836eac28d41756
sunil gupta <sunil.gupta@impetus.co.in>
2015-12-01 18:24:42 +0530

    Modified test cases

--- serviceplatformwebInterface/src/test/java/feature/sp/RunTest.java
+++ serviceplatformwebInterface/src/test/java/feature/sp/RunTest.java
@@ -2,9 +2,11 @@ package feature.sp;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@CucumberOptions(plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class RunTest {
 
 }
